568558706df1f8c707c0d6139e45a98a611d2d19
Erin Spruenken <48289831+spruenke@users.noreply.github.com>
2021-10-21 16:16:34 +0200

    update

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,49 +11,6 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _rankCluster_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _rankCluster_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _rankCluster_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _rankCluster_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 // f_psi_cpp
 arma::vec f_psi_cpp(arma::vec x, Rcpp::List data, arma::vec psi);
 RcppExport SEXP _rankCluster_f_psi_cpp(SEXP xSEXP, SEXP dataSEXP, SEXP psiSEXP) {
@@ -140,10 +97,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rankCluster_rcpparma_hello_world", (DL_FUNC) &_rankCluster_rcpparma_hello_world, 0},
-    {"_rankCluster_rcpparma_outerproduct", (DL_FUNC) &_rankCluster_rcpparma_outerproduct, 1},
-    {"_rankCluster_rcpparma_innerproduct", (DL_FUNC) &_rankCluster_rcpparma_innerproduct, 1},
-    {"_rankCluster_rcpparma_bothproducts", (DL_FUNC) &_rankCluster_rcpparma_bothproducts, 1},
     {"_rankCluster_f_psi_cpp", (DL_FUNC) &_rankCluster_f_psi_cpp, 3},
     {"_rankCluster_f_theta_cpp", (DL_FUNC) &_rankCluster_f_theta_cpp, 4},
     {"_rankCluster_rel_eff_cpp", (DL_FUNC) &_rankCluster_rel_eff_cpp, 3},
