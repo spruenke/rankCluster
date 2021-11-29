99f0f573290e3b7cccf51908416316c5e42d764f
Erin Spruenken <48289831+spruenke@users.noreply.github.com>
2021-11-29 16:58:25 +0100

    Add files via upload

--- R/data_gen.R
+++ R/data_gen.R
@@ -70,6 +70,10 @@ h_0_f = function(n = rep(5, 5), m = NULL, dist = "norm", corstruct = "independen
   return(l)
 }
 
+
+
+
+
 #' Function generating sample- and cluster sizes
 #' 
 #' @param nn Number of groups/samples
@@ -148,6 +152,90 @@ nm_gen = function(nn, n_i, m_ij, each_s = F, both_s = T, identical_s = T, identi
             m[[i]] = rep(m_ij, n[i])
           }
         }
+      }
+    }
+  }
+  return(list(n, m))
+}
+
+#' Function generating sample- and cluster sizes with only mild unbalancing
+#' 
+#' @param nn Number of groups/samples
+#' @param n_i Typical sample sizes
+#' @param m_ij Typical cluster size
+#' @param each_s Logical: Does each sample contain a large/small cluster? Default: FALSE
+#' @param both_s Logical: Does a sample contain both large/small clusters or only one of them? Default: TRUE
+#' @param identical_s Logical: Identical Sample sizes? Default: TRUE
+#' @param identical_c Logical: Identical Cluster sizes? Default: TRUE
+#' @return List containing the sample- and cluster sizes.
+nm_gen2 = function(nn, n_i, m_ij, each_s = F, both_s = T, identical_s = T, identical_c = T){
+  # each_s: Does each sample contain large/small cluster?
+  # both_s: Does a sample contain both large/small or only one?
+  # identical_s: Identical Sample Sizes?
+  # identical_c: Identical Clustersizes?
+  if(each_s == T && both_s == F) stop("Invalid conditions")
+  if(identical_s == T){
+    n = rep(n_i, nn)
+    m = list()
+    if(identical_c == T){
+      for(i in 1:nn){
+        m[[i]] = rep(m_ij, n_i)
+      }
+      
+    } else if(identical_c == F){
+      m_small = 3
+      m_large = 15
+      #if(each_s == F) other = sample(c(1:nn), 2)
+      #if(each_s == T) other = list(c(1:nn))
+      for(i in 1:nn){
+        #if(both_s == T){
+          #if(i %in% other[[1]]){
+            m[[i]] = sample(c(m_ij:(m_ij - 3), m_ij:(m_ij + 3)), size = n_i, replace = T)#c(m_small, m_large, rep(m_ij, (n_i-2))))
+          #} else {
+            #m[[i]] = rep(m_ij, n_i)
+          #}
+       # } else if(both_s == F){
+         # if(i == other[1]){
+            #m[[i]] = sample(c(m_small, rep(m_ij, (n_i-1))))
+         # } else if(i == other[2]){
+            #m[[i]] = sample(c(m_large, rep(m_ij, (n_i-1))))
+         # } else {
+            #m[[i]] = rep(m_ij, n_i)
+         # }
+        #}
+      }
+    }
+  }
+  
+  if(identical_s == F){
+    n = sample(c(n_i:(n_i - 3), n_i:(n_i + 3)), size = nn, replace = T)
+    m = list()
+    if(identical_c == T){
+      for(i in 1:nn){
+        m[[i]] = rep(m_ij, n_i)
+      }
+      
+    } else if(identical_c == F){
+      m_small = 3
+      m_large = 15
+      #if(each_s == F) other = sample(c(1:nn), 2)
+      #if(each_s == T) other = list(c(1:nn))
+      for(i in 1:nn){
+        #if(both_s == T){
+        #if(i %in% other[[1]]){
+        m[[i]] = sample(c(m_ij:(m_ij - 3), m_ij:(m_ij + 3)), size = n_i, replace = T)#c(m_small, m_large, rep(m_ij, (n_i-2))))
+        #} else {
+        #m[[i]] = rep(m_ij, n_i)
+        #}
+        # } else if(both_s == F){
+        # if(i == other[1]){
+        #m[[i]] = sample(c(m_small, rep(m_ij, (n_i-1))))
+        # } else if(i == other[2]){
+        #m[[i]] = sample(c(m_large, rep(m_ij, (n_i-1))))
+        # } else {
+        #m[[i]] = rep(m_ij, n_i)
+        # }
+        #}
       }
     }
   }
